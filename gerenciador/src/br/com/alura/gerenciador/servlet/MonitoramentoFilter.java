9caf8ae4b8b301d9f6d997ed020db34a6e9a0a8f
Eduardo <xeduxbrux@gmail.com>
2021-08-11 18:36:00 -0300

    add: adicionando filtros

--- gerenciador/src/br/com/alura/gerenciador/servlet/MonitoramentoFilter.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/MonitoramentoFilter.java
@@ -15,7 +15,10 @@ public class MonitoramentoFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		 long antes = System.currentTimeMillis();
+		 
+		System.out.println("MonitoramentoFilter");
+		
+		long antes = System.currentTimeMillis();
 		 
 		 String acao = request.getParameter("acao");
 		 
